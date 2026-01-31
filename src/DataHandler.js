600495c693efc24d2b33688c55d971e5a53637a5
Yujin <yujinpetercho@gmail.com>
2026-01-31 05:17:51 -0600

    seperated version upgrades into handleVersionUpgrades method

--- src/DataHandler.js
+++ src/DataHandler.js
@@ -3,6 +3,61 @@
 class DataHandler {
     database = null;
 
+    async handleVersionUpgrades(event) {
+        this.database = event.target.result;
+        const oldVersion = event.oldVersion;
+
+        if (oldVersion < 1) {
+            const tasksObjectStore = this.database.createObjectStore("tasks", { keyPath: "createdAt"});
+            tasksObjectStore.createIndex("taskName", "taskName", { unique:false });
+
+        }else if (oldVersion < 2) {
+            this.database = event.target.result;
+            const transaction = event.target.transaction; //special type of transaction for versionchange
+            const tasksObjectStore = transaction.objectStore("tasks");
+            
+            tasksObjectStore.createIndex("location", "location");
+            tasksObjectStore.createIndex("distractions", "distractions");
+            tasksObjectStore.createIndex("similarity", "similarity");
+            tasksObjectStore.createIndex("timeOfStart", "timeOfStart");
+            tasksObjectStore.createIndex("reasonToSelect", "reasonToSelect");
+            tasksObjectStore.createIndex("efficiency", "efficiency");
+            tasksObjectStore.createIndex("estimatedDuration", "estimatedDuration");
+            tasksObjectStore.createIndex("estimatedBuffer", "estimatedBuffer");
+
+            tasksObjectStore.oncomplete = (event) => {
+                const tasksObjectStore = this.database
+                    .transaction("tasks", "readwrite")
+                    .objectStore("tasks");
+                
+                    const getAllRequest = tasksObjectStore.getAll();
+
+                    getAllRequest.oncomplete = () => {
+                        const tasks = getAllRequest.result;
+
+                        tasks.forEach((task) => {
+                            task.location = task.location || "";
+                            task.distractions = task.distractions || "";
+                            task.similarity = task.similarity || "";
+                            task.timeOfStart = task.timeOfStart || "";
+                            task.reasonToSelect = task.reasonToSelect || "";
+                            task.efficiency = task.efficiency || "";
+                            task.estimatedDuration = task.estimatedDuration || "";
+                            task.estimatedBuffer = task.estimatedBuffer || "";
+                            
+                            tasksObjectStore.put(task);
+                        })
+                    }
+            }
+            
+        /** to store (existing) data
+            https://developer.mozilla.org/en-US/docs/Web/API/IndexedDB_API/Using_IndexedDB
+        */  
+        }else if (oldVersion < 3) {
+
+        }
+    }
+
     constructor() {
         if (!this.isCompatable()) {
             alert("Browser incompatability with IndexDB.");
@@ -16,58 +71,8 @@ class DataHandler {
                 reject(request.error);
             }
 
-            request.onupgradeneeded = (event) => {
-                //in future versions - do not recreate object stores that exist.
-                this.database = event.target.result;
-                const oldVersion = event.oldVersion;
-
-                if (oldVersion < 1) {
-                    const tasksObjectStore = this.database.createObjectStore("tasks", { keyPath: "createdAt"});
-                    tasksObjectStore.createIndex("taskName", "taskName", { unique:false });
-
-                }else if (oldVersion < 2) {
-                    this.database = event.target.result;
-                    const transaction = event.target.transaction; //special type of transaction for versionchange
-                    const tasksObjectStore = transaction.objectStore("tasks");
-                    
-                    tasksObjectStore.createIndex("location", "location");
-                    tasksObjectStore.createIndex("distractions", "distractions");
-                    tasksObjectStore.createIndex("similarity", "similarity");
-                    tasksObjectStore.createIndex("timeOfStart", "timeOfStart");
-                    tasksObjectStore.createIndex("reasonToSelect", "reasonToSelect");
-                    tasksObjectStore.createIndex("efficiency", "efficiency");
-                    tasksObjectStore.createIndex("estimatedDuration", "estimatedDuration");
-                    tasksObjectStore.createIndex("estimatedBuffer", "estimatedBuffer");
-
-                    tasksObjectStore.oncomplete = (event) => {
-                        const tasksObjectStore = this.database
-                            .transaction("tasks", "readwrite")
-                            .objectStore("tasks");
-                        
-                            const getAllRequest = tasksObjectStore.getAll();
-
-                            getAllRequest.oncomplete = () => {
-                                const tasks = getAllRequest.result;
-
-                                tasks.forEach((task) => {
-                                    task.location = task.location || "";
-                                    task.distractions = task.distractions || "";
-                                    task.similarity = task.similarity || "";
-                                    task.timeOfStart = task.timeOfStart || "";
-                                    task.reasonToSelect = task.reasonToSelect || "";
-                                    task.efficiency = task.efficiency || "";
-                                    task.estimatedDuration = task.estimatedDuration || "";
-                                    task.estimatedBuffer = task.estimatedBuffer || "";
-                                    
-                                    tasksObjectStore.put(task);
-                                })
-                            }
-                    }
-                    
-                /** to store (existing) data
-                    https://developer.mozilla.org/en-US/docs/Web/API/IndexedDB_API/Using_IndexedDB
-                */  
-                }
+            request.onupgradeneeded = async (event) => {
+                await this.handleVersionUpgrades(event);
             }
 
             request.onsuccess = (event) => {
